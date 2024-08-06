998eaa0e65a5dcf566185cc6c88640fa1edeeea9
ndizeyedavid <davidndizeye101@gmail.com>
2024-08-06 13:40:21 +0300

    this

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.use(cors());
 
 app.use(express.json());
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "esp"
+})
+
 
 app.listen(port, ()=>{
     console.log("App running on port ", port);
