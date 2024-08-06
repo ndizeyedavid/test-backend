c823b8c4ae1bbe7c9db29e96063f8810ea9cf165
ndizeyedavid <davidndizeye101@gmail.com>
2024-08-06 13:21:34 +0300

    upd

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 import express from 'express';
 import cors from 'cors';
-
+import mysql from 'mysql2'
 const port = 9090; // now don't judge me, i was just testing lol😁
 const app = express();
 app.use(cors());
 
 app.use(express.json());
-
-import chance from 'chance';
-
-const chances = new chance();
-
-const animals = [...Array(150).keys()].map(id =>{
-    return{
-        id, 
-        type: chances.animal(),
-        age: chances.age(),
-        name: chances.name(),
-    }
-});
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "esp"
+})
 
 
-app.get('', (req, res)=>{
+app.get('/data', (req, res)=>{
+    const sql = "SELECT * FROM SensorData";
+    db.query(sql, (err, data)=>{
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+})
 
-    const q = req.query.q?.toLocaleLowerCase()  || '';
-    const results = animals.filter(animal => animal.type.toLocaleLowerCase().includes(q));
-
-    res.send(results);
-    
+app.get('/average', (req, res)=>{
+    const sql = "SELECT ROUND(AVG(value1), 1) as value1, ROUND(AVG(value2), 1) as value2, ROUND(AVG(value3), 1) as value3 FROM sensordata;"
+    db.query(sql, (err, data)=>{
+        if (err) return res.json(err);
+        return res.json(data);
+    })
 });
 
 app.listen(port, ()=>{
